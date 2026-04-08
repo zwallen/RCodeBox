32e7b8ce2f63cb84dbdad38ef33e4cc13a325cc8
zwallen <zachary.d.wallen@gmail.com>
2026-04-08 14:55:39 -0400

    fixing import and namespace issues

--- R/RCodeBox-package.R
+++ R/RCodeBox-package.R
@@ -1,4 +1,8 @@
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+## usethis namespace: start
 #' @useDynLib RCodeBox, .registration = TRUE
 ## usethis namespace: end
+NULL
